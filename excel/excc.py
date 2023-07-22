2298bcda60dfd2aff07f053d517a84a63dfd8925
JeonHR <cjstkqud296@gmail.com>
2023-07-22 21:03:01 +0900

    second commit

--- excel/excc.py
+++ excel/excc.py
@@ -8,11 +8,12 @@ excel_file = os.path.join(current_directory, folder_path, 'ko.xlsx')
 df = pd.read_excel(excel_file, sheet_name = 'ko', engine= 'openpyxl')
 print (df)
 
+hbhbh
 
 start_row = 1
 end_row = 10
 start_column = 1
-end_column = 10
+end_column = d=
 selected_data = df.loc['start_row':'end_row', 'start_column':'end_column']
 
 df(selected_data)
