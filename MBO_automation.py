2346984f06879aa2f431a8fe7401c1c0b8521b13
JeonHR <cjstkqud296@gmail.com>
2023-07-23 23:35:56 +0900

    third file

--- MBO_automation.py
+++ MBO_automation.py
@@ -1,3 +1,26 @@
+import os
+import pandas as pd
+import time 
+import re
+
+
+current_directory = os.getcwd()
+unused_folder_path = "./unused_txt"
+
+## folder in all file list up
+all_unused_txt = os.listdir(unused_folder_path)
+
+## Delete folder in txt.file 
+for txt_name in all_unused_txt:
+    if txt_name.endswith(".txt"):
+        unused_file_path = os.path.join(unused_folder_path, txt_name)
+        os.remove(unused_file_path) 
+
+
+time.sleep (2)
+print("unused txt file이 삭제 완료 되었습니다. ")
+
+########################################
 
 # 특정 폴더 경로와 병합 결과를 저장할 파일명 설정
 Merged_folder_path = "./"  # 특정 경로
@@ -26,9 +49,20 @@ def merge_txt_files(Merged_folder_path, output_file):
 
     print(f"모든 .txt 파일을 {output_file}에 병합하여 저장하였습니다.")
 
-# 특정 폴더 경로와 병합 결과를 저장할 파일명 설정
-Merged_folder_path = "./"  # 특정 경로
-output_file = "merged.txt"  # 병합한 결과를 저장할 파일
+# Merge file & path 
+Merged_folder_path = "./"  # merge file path
+output_file = "merged.txt"  # Result txt file
 
-# 병합 함수 호출
+# Merge txt file 
 merge_txt_files(Merged_folder_path, output_file)
+
+##############################################
+
+
+
+print(current_directory)
+
+folder_path = "./excel"
+excel_file = os.path.join(current_directory, folder_path, 'ko.xlsx')
+df = pd.read_excel(excel_file, sheet_name = 'ko')
+print (df)
--- excel/excc.py
+++ excel/excc.py
@@ -1,26 +1,20 @@
 import os
 import pandas as pd
 
+
+
+
 current_directory = os.getcwd()
+
+
+print(current_directory)
 
 folder_path = "./excel"
 excel_file = os.path.join(current_directory, folder_path, 'ko.xlsx')
-df = pd.read_excel(excel_file, sheet_name = 'ko', engine= 'openpyxl')
+df = pd.read_excel(excel_file, sheet_name = 'ko')
 print (df)
 
-hbhbh
 
-start_row = 1
-end_row = 10
-start_column = 1
-end_column = d=
-selected_data = df.loc['start_row':'end_row', 'start_column':'end_column']
-
-df(selected_data)
-
-
-txt_file = os.path.join(current_directory, folder_path, 'save.txt')
-selected_data.to_xlsl(txt_file, sep='\t', index=False)
 
 
 print("변환 완료!")
